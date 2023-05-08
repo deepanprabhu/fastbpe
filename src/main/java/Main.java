fc4d8c933913ab9cb12eb1b8e781b0d2f141237e
deepanprabhu <deepan18@gmail.com>
2023-05-08 06:26:38 -0700

    Refactoring and bug fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,15 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+/**
+ * words are from texts
+ * vocabulary are individual chars from words
+ */
 public class Main {
     public static final String doc_gpt = "Soviet microbattery (generally, 3 watts) or commercially powered 60 Volt battery of 30 watts will run you between $25-30, if you pay 50 cents per pound for full ownership. The only drawback of lesser battery costs is of course that those who use them end up paying more. Just a reminder: Soviet and government subsidized, solid-state state power stations incurs a 10% mechanical surcharge being now the EU national minimum until around 2030. Right now, all German utilities collect virtually no electricity and are its biggest offenders. Energy resources will certainly not be magically starved out until 2061. Meanwhile, USSR electricity grids that can be developed quickly can be trusted to hold onto most of the industrial power.\\n\\nThere are four different types of parallel grounders (pow-powered), with the main family being the steppe type. Manhattan produced its first light bulb around 100 years ago and Stalin rye maker Ural had its first doubled capacity long after sawing the old white barn in 1928. Then a fusion of hydrogen and uranium captured power at the very lowest electrical teragrams on the grid with nuclear explosives.";
 
     public static void main(String[] args) {
-        List<String> tokenizedText = get_tokenized_text(doc_gpt);
+        List<String> wordsWithSpace = getTokenizedWordsWithSpace(doc_gpt);
 
         //Alphabets
         Set<String> vocabulary = Stream.of(
@@ -29,28 +33,27 @@ public class Main {
         Map<String, Integer> vocab_count = new HashMap<>();
 
         //word count
-        updateTokenTextWordCount(tokenizedText, word_count);
+        updateWordCount(wordsWithSpace, word_count);
 
 
-        Set<String> ordered_vocabulary = new HashSet<>();
+        Set<String> ordered_vocabulary;
         ordered_vocabulary = reOrderVocabularyByCount(vocabulary, vocab_count);
 
-        vocab_count = new HashMap<>();
-        countVocabulary(tokenizedText, vocab_count, ordered_vocabulary);
-
+        //count and reorder
+        countVocabulary(wordsWithSpace, ordered_vocabulary, vocab_count);
         ordered_vocabulary = reOrderVocabularyByCount(vocabulary, vocab_count);
 
         int numMerges = 20;
         while(numMerges-- > 0) {
             List<String> topPair = new ArrayList<>();
-            pair_count = countPairs(tokenizedText, vocab_count, topPair);
-            System.out.println("topPair " + topPair.get(0) + " " + topPair.get(1));
-            mergePairInVocabulary(tokenizedText, topPair, word_count, vocab_count, pair_count);
+            pair_count = countPairs(wordsWithSpace, vocab_count, topPair);
+            System.out.println("topPair <>" + topPair.get(0) + "<>" + topPair.get(1) + "<>");
+            mergePairInVocabulary(wordsWithSpace, topPair, word_count, vocab_count, pair_count);
+            countVocabulary(wordsWithSpace, ordered_vocabulary, vocab_count);
         }
-        System.out.println("");
     }
 
-    private static void updateTokenTextWordCount(List<String> tokenizedText, Map<String, Integer> word_count) {
+    private static void updateWordCount(List<String> tokenizedText, Map<String, Integer> word_count) {
         word_count.clear();
         tokenizedText
                 .stream()
@@ -92,21 +95,17 @@ public class Main {
     private static Map<String, Integer> countPairs(List<String> tokenizedText, Map<String, Integer> vocab_count,
                                                    List<String> topPair) {
         Map<String, Integer> pair_count = new HashMap<>();
-
-
         int maxPair = Integer.MIN_VALUE;
 
         for (String word : tokenizedText) {
-            System.out.println(word);
             String[] splitWord = word.split(" ");
 
             for (int i = 0; i < splitWord.length - 1; i++) {
                 String key = String.format("%s %s ", splitWord[i], splitWord[i + 1]);
-
                 pair_count.put(key,
                         pair_count.getOrDefault(key, 0)
-                                + vocab_count.get(splitWord[i] + " ")
-                                + vocab_count.get(splitWord[i + 1] + " ")
+                                + vocab_count.getOrDefault(splitWord[i] + " ", 0)
+                                + vocab_count.getOrDefault(splitWord[i + 1] + " ", 0)
                 );
 
                 if (pair_count.get(key) > maxPair) {
@@ -117,7 +116,6 @@ public class Main {
                 }
             }
         }
-        ;
 
         return pair_count;
     }
@@ -126,7 +124,8 @@ public class Main {
         doc_gpt.chars().filter(c -> !vocabulary.contains(c)).forEach(c -> vocabulary.add(Character.toString(c) + " "));
     }
 
-    private static void countVocabulary(List<String> tokenizedText, Map<String, Integer> vocab_count, Set<String> ordered_vocabulary) {
+    private static void countVocabulary(List<String> tokenizedText, Set<String> ordered_vocabulary, Map<String, Integer> vocab_count) {
+        vocab_count.clear();
         tokenizedText.stream().forEach(w -> {
             String tw = String.copyValueOf(w.toCharArray());
 
@@ -159,7 +158,7 @@ public class Main {
         return ordered_vocabulary;
     }
 
-    private static List<String> get_tokenized_text(String doc_gpt) {
+    private static List<String> getTokenizedWordsWithSpace(String doc_gpt) {
         Properties props = new Properties();
         props.setProperty("annotators", "tokenize");
         props.setProperty("tokenize.options", "splitHyphenated=false,americanize=false");
