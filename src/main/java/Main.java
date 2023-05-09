5db31f6bf8c5cf51ec06584eddd633abd3892fb8
deepanprabhu <deepan18@gmail.com>
2023-05-09 11:22:18 -0700

    Bug fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,11 @@ public class Main {
     public static final String doc_gpt = "Soviet microbattery (generally, 3 watts) or commercially powered 60 Volt battery of 30 watts will run you between $25-30, if you pay 50 cents per pound for full ownership. The only drawback of lesser battery costs is of course that those who use them end up paying more. Just a reminder: Soviet and government subsidized, solid-state state power stations incurs a 10% mechanical surcharge being now the EU national minimum until around 2030. Right now, all German utilities collect virtually no electricity and are its biggest offenders. Energy resources will certainly not be magically starved out until 2061. Meanwhile, USSR electricity grids that can be developed quickly can be trusted to hold onto most of the industrial power.\\n\\nThere are four different types of parallel grounders (pow-powered), with the main family being the steppe type. Manhattan produced its first light bulb around 100 years ago and Stalin rye maker Ural had its first doubled capacity long after sawing the old white barn in 1928. Then a fusion of hydrogen and uranium captured power at the very lowest electrical teragrams on the grid with nuclear explosives.";
 
     public static void main(String[] args) {
-        List<String> wordsWithSpace = getTokenizedWordsWithSpace(doc_gpt);
+        Map<String, Integer> pair_count;
+        Map<String, Integer> word_count = new HashMap<>();
+        Map<String, Integer> vocab_count = new HashMap<>();
+        Set<String> ordered_vocabulary;
+        List<String> topPair;
 
         //Alphabets
         Set<String> vocabulary = Stream.of(
@@ -26,34 +30,33 @@ public class Main {
                 .map(c -> Character.toString(c) + " ")
                 .collect(Collectors.toSet());
 
-        addToVocabularyFromText(doc_gpt, vocabulary);
 
-        Map<String, Integer> pair_count;
-        Map<String, Integer> word_count = new HashMap<>();
-        Map<String, Integer> vocab_count = new HashMap<>();
+        List<String> wordsWithSpace = getTokenizedWordsWithSpace(doc_gpt);
+
+        addVocabFromText(doc_gpt, vocabulary);
 
         //word count
-        updateWordCount(wordsWithSpace, word_count);
+        calculateWordCount(wordsWithSpace, word_count);
 
-
-        Set<String> ordered_vocabulary;
         ordered_vocabulary = reOrderVocabularyByCount(vocabulary, vocab_count);
 
         //count and reorder
         countVocabulary(wordsWithSpace, ordered_vocabulary, vocab_count);
         ordered_vocabulary = reOrderVocabularyByCount(vocabulary, vocab_count);
 
-        int numMerges = 20;
+        int numMerges = 100;
         while(numMerges-- > 0) {
-            List<String> topPair = new ArrayList<>();
-            pair_count = countPairs(wordsWithSpace, vocab_count, topPair);
-            System.out.println("topPair <>" + topPair.get(0) + "<>" + topPair.get(1) + "<>");
-            mergePairInVocabulary(wordsWithSpace, topPair, word_count, vocab_count, pair_count);
-            countVocabulary(wordsWithSpace, ordered_vocabulary, vocab_count);
+            topPair = new ArrayList<>();
+            pair_count = countPairsAndFindBest(wordsWithSpace, word_count, topPair);
+
+            if(topPair.size() > 0) {
+                mergePairInVocabulary(wordsWithSpace, topPair, word_count, vocab_count, pair_count);
+                countVocabulary(wordsWithSpace, ordered_vocabulary, vocab_count);
+            }
         }
     }
 
-    private static void updateWordCount(List<String> tokenizedText, Map<String, Integer> word_count) {
+    private static void calculateWordCount(List<String> tokenizedText, Map<String, Integer> word_count) {
         word_count.clear();
         tokenizedText
                 .stream()
@@ -92,23 +95,20 @@ public class Main {
         }
     }
 
-    private static Map<String, Integer> countPairs(List<String> tokenizedText, Map<String, Integer> vocab_count,
-                                                   List<String> topPair) {
+    private static Map<String, Integer> countPairsAndFindBest(List<String> tokenizedText, Map<String, Integer> word_count,
+                                                              List<String> topPair) {
         Map<String, Integer> pair_count = new HashMap<>();
         int maxPair = Integer.MIN_VALUE;
 
         for (String word : tokenizedText) {
             String[] splitWord = word.split(" ");
+            //System.out.println(word + " <> " + word_count.get(word) );
 
             for (int i = 0; i < splitWord.length - 1; i++) {
                 String key = String.format("%s %s ", splitWord[i], splitWord[i + 1]);
-                pair_count.put(key,
-                        pair_count.getOrDefault(key, 0)
-                                + vocab_count.getOrDefault(splitWord[i] + " ", 0)
-                                + vocab_count.getOrDefault(splitWord[i + 1] + " ", 0)
-                );
+                pair_count.put(key, pair_count.getOrDefault(key, 0) + 1);
 
-                if (pair_count.get(key) > maxPair) {
+                if (pair_count.get(key) > maxPair && pair_count.get(key) > 0) {
                     maxPair = pair_count.get(key);
                     topPair.clear();
                     topPair.add(splitWord[i] + " ");
@@ -117,10 +117,13 @@ public class Main {
             }
         }
 
+        if(topPair.size() > 0)
+            System.out.println(String.format("Top pair - <>%s<>%s - %d", topPair.get(0),
+                topPair.get(1), pair_count.get(topPair.get(0)+topPair.get(1))));
         return pair_count;
     }
 
-    private static void addToVocabularyFromText(String doc_gpt, Set<String> vocabulary) {
+    private static void addVocabFromText(String doc_gpt, Set<String> vocabulary) {
         doc_gpt.chars().filter(c -> !vocabulary.contains(c)).forEach(c -> vocabulary.add(Character.toString(c) + " "));
     }
 
