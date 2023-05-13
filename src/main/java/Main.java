f0c8a7dc38af90e84021819030e433bf289a26bc
deepanprabhu <deepan18@gmail.com>
2023-05-13 10:50:35 -0700

    working bpe tokenization - but output differs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,8 +11,8 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
- * words are from texts
- * vocabulary are individual chars from words
+ * Words are from texts
+ * Vocabulary are individual chars from words
  */
 public class Main {
     //public static final String doc_gpt = "Soviet microbattery (generally, 3 watts) or commercially powered 60 Volt battery of 30 watts will run you between $25-30, if you pay 50 cents per pound for full ownership. The only drawback of lesser battery costs is of course that those who use them end up paying more. Just a reminder: Soviet and government subsidized, solid-state state power stations incurs a 10% mechanical surcharge being now the EU national minimum until around 2030. Right now, all German utilities collect virtually no electricity and are its biggest offenders. Energy resources will certainly not be magically starved out until 2061. Meanwhile, USSR electricity grids that can be developed quickly can be trusted to hold onto most of the industrial power.\\n\\nThere are four different types of parallel grounders (pow-powered), with the main family being the steppe type. Manhattan produced its first light bulb around 100 years ago and Stalin rye maker Ural had its first doubled capacity long after sawing the old white barn in 1928. Then a fusion of hydrogen and uranium captured power at the very lowest electrical teragrams on the grid with nuclear explosives.";
@@ -61,6 +61,7 @@ public class Main {
 
         ordered_vocabulary = reOrderVocabularyByLength(vocabulary);
         tokenizeText("New soviet powered commercial micro battery", ordered_vocabulary);
+        tokenizeText("mountains ced however rench pathetic", ordered_vocabulary);
     }
 
     private static List<String> tokenizeText(String text, Set<String> ordered_vocabulary) {
@@ -150,7 +151,7 @@ public class Main {
         }
 
         if(topPair.size() > 0) {
-            System.out.println(String.format("Top pair - <>%s<>%s - %d", topPair.get(0), topPair.get(1), pair_count.get(topPair.get(0) + topPair.get(1) + TOKEN_DELIMITER)));
+            //System.out.println(String.format("Top pair - <>%s<>%s - %d", topPair.get(0), topPair.get(1), pair_count.get(topPair.get(0) + topPair.get(1) + TOKEN_DELIMITER)));
         }
         return pair_count;
     }
